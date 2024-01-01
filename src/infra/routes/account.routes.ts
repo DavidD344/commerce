1c0afe73f85f516550564a77dbcdf5ab3512be1f
DavidManj <davidsantosfreitas13@gmail.com>
2024-01-01 14:57:24 -0300

    change register for login

--- src/infra/routes/account.routes.ts
+++ src/infra/routes/account.routes.ts
@@ -1,29 +1,16 @@
-import { LoginUserController } from '@/presentation/controllers/user/RegisterUserController'
+import { LoginUserController } from '@/presentation/controllers/user/LoginUserController'
 import { type Request, type Response, Router, type RequestHandler } from 'express'
 
 const loginUserController = new LoginUserController()
 
 const accountRoutes = Router()
-
-accountRoutes.get('/create', ({ res }) => { res?.send('create') })
-accountRoutes.get('/delete', ({ res }) => { res?.send('delete') })
-
+accountRoutes.get('/register', (async (req: Request, res: Response) => {
+  await loginUserController.handler(req, res)
+}) as RequestHandler
+)
 accountRoutes.get('/login', (async (req: Request, res: Response) => {
-  console.log('passei')
   await loginUserController.handler(req, res)
 }) as RequestHandler
 )
 
 export default accountRoutes
-
-// const deleteAccount = (req: Request, res: Response): void => {
-//   try {
-//     // Aqui você pode adicionar a lógica para exclusão da conta
-//     console.log('Requisição:', req)
-//     console.log('Resposta:', res)
-
-//     res.status(200).json({ message: 'Conta excluída com sucesso.' })
-//   } catch (err) {
-//     res.status(500).json({ message: 'Erro ao processar a exclusão da conta.', error: err })
-//   }
-// }
--- src/presentation/controllers/user/LoginUserController.ts
+++ src/presentation/controllers/user/LoginUserController.ts
@@ -8,8 +8,6 @@ export class LoginUserController {
     // res.send('De dentro do controller')
     const data = { email: 'a@gmail.com', password: '123' }
     const token: UserAuthentication = await this.loginUserHandler.login(data)
-    console.log(token)
-    // return res.send(token.jwt)
     return res.send(`De dentro do controller ${token.jwt}`)
   }
 }
